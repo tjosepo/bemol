217d3b1b0c6f21efcbff938fbb9da92970c0b3e3
Tommy Josépovic <tommy.josepovic@gmail.com>
2021-03-07 16:34:03 -0500

    Add middlewares

--- debug/Debug.cs
+++ debug/Debug.cs
@@ -1,7 +1,28 @@
 using Bemol;
+using Bemol.Middlewares;
+
+using System;
 
 using Wesbite;
 
 App app = new App().Start();
-app.Get("/", ctx => ctx.Render("index.liquid"));
-app.Use(new Auth().Router);
+
+app.Get("/", ctx => {
+    System.Console.WriteLine("endpoint handler");
+    ctx.Render("index.liquid");
+});
+app.Post("/", ctx => ctx.Render("index.liquid"));
+// app.Use(new Auth().Router);
+
+app.Use(Logger.New());
+app.Use((ctx) => {
+    var start = DateTime.Now;
+    ctx.Finally(() => {
+        var ms = DateTime.Now - start;
+        Console.WriteLine("Response time:" + ms);
+    });
+});
+
+
+
+
